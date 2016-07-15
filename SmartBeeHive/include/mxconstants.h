727541d7a507f418ee0c24050b1206161e573766
Csatacsibe <kaulics.bence@gmail.com>
2016-07-15 17:27:25 +0200

    Created STM32F030x6 STM-HAL Eclipse Project

---
+++ SmartBeeHive/include/mxconstants.h
@@ -0,0 +1,83 @@
+/**
+  ******************************************************************************
+  * File Name          : mxconstants.h
+  * Description        : This file contains the common defines of the application
+  ******************************************************************************
+  *
+  * COPYRIGHT(c) 2016 STMicroelectronics
+  *
+  * Redistribution and use in source and binary forms, with or without modification,
+  * are permitted provided that the following conditions are met:
+  *   1. Redistributions of source code must retain the above copyright notice,
+  *      this list of conditions and the following disclaimer.
+  *   2. Redistributions in binary form must reproduce the above copyright notice,
+  *      this list of conditions and the following disclaimer in the documentation
+  *      and/or other materials provided with the distribution.
+  *   3. Neither the name of STMicroelectronics nor the names of its contributors
+  *      may be used to endorse or promote products derived from this software
+  *      without specific prior written permission.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+  * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+  * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+  * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+  * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+/* Includes ------------------------------------------------------------------*/
+
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Private define ------------------------------------------------------------*/
+
+#define SIM_RST_Pin GPIO_PIN_0
+#define SIM_RST_GPIO_Port GPIOF
+#define SIM_PWR_KEY_Pin GPIO_PIN_1
+#define SIM_PWR_KEY_GPIO_Port GPIOF
+#define GSM_RI_Pin GPIO_PIN_0
+#define GSM_RI_GPIO_Port GPIOA
+#define GPS_ANT_EN_Pin GPIO_PIN_1
+#define GPS_ANT_EN_GPIO_Port GPIOA
+#define GAUGE_OUT_Pin GPIO_PIN_4
+#define GAUGE_OUT_GPIO_Port GPIOA
+#define I_SUPP_Pin GPIO_PIN_5
+#define I_SUPP_GPIO_Port GPIOA
+#define V_BAT_Pin GPIO_PIN_6
+#define V_BAT_GPIO_Port GPIOA
+#define CHG_STAT_Pin GPIO_PIN_0
+#define CHG_STAT_GPIO_Port GPIOB
+#define CHG_EN_Pin GPIO_PIN_1
+#define CHG_EN_GPIO_Port GPIOB
+#define _4_2V_EN_Pin GPIO_PIN_8
+#define _4_2V_EN_GPIO_Port GPIOA
+#define GY_RST_Pin GPIO_PIN_3
+#define GY_RST_GPIO_Port GPIOB
+#define GY_INT1_Pin GPIO_PIN_4
+#define GY_INT1_GPIO_Port GPIOB
+#define GY_INT2_Pin GPIO_PIN_5
+#define GY_INT2_GPIO_Port GPIOB
+#define RSVD_Pin GPIO_PIN_6
+#define RSVD_GPIO_Port GPIOB
+#define RSVDB7_Pin GPIO_PIN_7
+#define RSVDB7_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+/**
+  * @}
+  */ 
+
+/**
+  * @}
+*/ 
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
