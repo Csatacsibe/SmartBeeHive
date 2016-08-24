7ccb2b7d255bf8d6ecb425334d355631801c61a6
Csatacsibe <kaulics.bence@gmail.com>
2016-08-24 21:01:11 +0200

    VBAT, MCU VDD, board supply current readings (related ADC init)

--- include/STM32_bsp/adc.h
+++ include/STM32_bsp/adc.h
@@ -12,10 +12,21 @@
 #endif
 
 #include "stm32f0xx_hal.h"
+#include "types.h"
+
+#define STRAIN_GAUGE    ADC_CHANNEL_4
+#define SUPPLY_CURRENT  ADC_CHANNEL_5
+#define BATTERY_VOLTAGE ADC_CHANNEL_6
 
 extern ADC_HandleTypeDef hadc;
 
 void MX_ADC_Init(void);
+
+void config_ext_channel_ADC(uint32_t channel, boolean_t val);
+uint32_t r_single_ext_channel_ADC(uint32_t channel);
+
+void config_int_channel_ADC(uint32_t channel, boolean_t val);
+uint32_t r_single_int_channel_ADC(uint32_t channel);
 
 #ifdef __cplusplus
 }
