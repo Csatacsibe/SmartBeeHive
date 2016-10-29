61794a3eea2ab32f951bfb2ba25046d884e17ba4
ebenkau <bence.kaulics@ericsson.com>
2016-10-29 21:21:15 +0200

    updated useful comment list

--- include/GPRS_GSM_GPS/SIM808_device.h
+++ include/GPRS_GSM_GPS/SIM808_device.h
@@ -16,6 +16,10 @@
  * AT+CMTE Set Critical Temperature Operating Mode or Query Temperature
  * AT+SLEDS Set the Timer Period of Net Light
  * AT+CSGS Netlight Indication of GPRS Status
+ * AT+CNETLIGHT Close the Net Light or Open It to Shining
+ * AT+CWHITELIST Set the White List
+ * AT+CSMINS SIM Inserted Status Reporting
+ * AT+CIPGSMLOC GSM Location and Time
  */
 
 /*
