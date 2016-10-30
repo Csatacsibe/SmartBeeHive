ac055ca4e13ae56f1d0960c1c3728e21d5a54879
ebenkau <bence.kaulics@ericsson.com>
2016-10-30 12:50:44 +0100

    added GPS/GNSS commands into GPS header

--- include/GPRS_GSM_GPS/SIM808_GPS.h
+++ include/GPRS_GSM_GPS/SIM808_GPS.h
@@ -8,6 +8,22 @@
 #ifndef GPRS_GSM_GPS_SIM808_GPS_H_
 #define GPRS_GSM_GPS_SIM808_GPS_H_
 
+/*
+ * AT+CGPSPWR    GPS Power Control
+ * AT+CGPSRST    GPS Reset Mode (COLD/HOT/WARM)
+ * AT+CGPSINF    Get Current GPS Location Info
+ * AT+CGPSOUT    GPS NMEA Data Output Control
+ * AT+CGPSSTATUS GPS Status
+ *
+ *            OR
+ *
+ * AT+CGNSPWR GNSS power control
+ * AT+CGNSSEQ Define the last NMEA sentence that parsed
+ * AT+CGNSINF GNSS navigation information parsed from NMEA sentences
+ * AT+CGNSURC GNSS navigation, GEO-fences and speed alarm URC report
+ * AT+CGNSCMD Send command to GNSS
+ * AT+CGNSTST Send data received from GNSS to AT UART
+ */
 
 
 #endif /* GPRS_GSM_GPS_SIM808_GPS_H_ */
