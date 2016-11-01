52db47f108eaa01a0b430068c8df97be400d9e63
Csatacsibe <kaulics.bence@gmail.com>
2016-11-01 19:26:59 +0100

    I2C standard mode

--- src/STM32_bsp/i2c.c
+++ src/STM32_bsp/i2c.c
@@ -13,7 +13,7 @@ I2C_HandleTypeDef hi2c1;
 void MX_I2C1_Init(void)
 {
   hi2c1.Instance = I2C1;
-  hi2c1.Init.Timing = 0x1042110F;//0x10420F13;//0x10421113//0x2000090E;
+  hi2c1.Init.Timing = 0x1042110F;
   hi2c1.Init.OwnAddress1 = 0;
   hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
   hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLED;
