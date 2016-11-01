260c616be89d5848d036c3681058fd8876428906
Csatacsibe <kaulics.bence@gmail.com>
2016-11-01 12:45:53 +0100

    SMS sending is incomplete due to changed UART RX mechanism

--- src/GPRS_GSM_GPS/SIM808_GSM.c
+++ src/GPRS_GSM_GPS/SIM808_GSM.c
@@ -8,20 +8,18 @@
 #include "GPRS_GSM_GPS/SIM808_GSM.h"
 #include "GPRS_GSM_GPS/SIM808_driver.h"
 
-#include "string.h"
-
 uint8_t send_SMS(char* text, char* number)
 {
-  if(strlen(text) > 160)
+  if(length(text) > 160)
   {
     return 0;
   }
 
   char cmd[30];
-  char ctrlZ = 0x1A; // <Ctrl + Z> in ASCII
+  //char ctrlZ = 0x1A; // <Ctrl + Z> in ASCII
 
   sprintf(cmd,"%s=\"%s\"\r","AT+CMGS",number);
-  if(check_resp_SIM808(cmd, ">", 1, 500))
+  /*if(check_resp_SIM808(cmd, ">", 1, 500))
   {
     put_s_SIM808(text);
     if(send_n_wait_for_resp(&ctrlZ, 13, 500))
@@ -30,7 +28,7 @@ uint8_t send_SMS(char* text, char* number)
       return 1;
     }
 
-  }
+  }*/
 
   return 0;
 }
